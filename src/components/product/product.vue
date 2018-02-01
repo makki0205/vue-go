d9f6d9da958d8aff633f4ee99bbbba84cedbed2a
konojunya <junya03dance@gmail.com>
2018-02-01 16:24:15 +0900

    Vueのプロジェクト開始

--- src/components/product/product.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <h1>プロダクトだお</h1>
-</template>
-<script>
-    export default {
-        name: "notFound"
-    }
-</script>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Index from './components/index/index.vue'
-import Product from './components/product/product.vue'
 import NotFound from './components/notFound/notFound.vue'
 
 Vue.component('index', Index)
@@ -10,7 +9,6 @@ Vue.use(VueRouter)
 
 const routes = [
     { path: "/", component: Index },
-    { path: "/m*", component: Product },
     { path: "*", component: NotFound },
 ]
 const router = new VueRouter({  mode: 'history', routes })
