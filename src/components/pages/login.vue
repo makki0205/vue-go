eaf8866e9dbd5fa7b654bd3eaeb25ef01b26879e
makki0205 <llxo2_5oxll@icloud.com>
2018-02-06 17:27:27 +0900

    .

---
+++ src/components/pages/login.vue
@@ -0,0 +1,40 @@
+<template>
+    <form action="">
+        <div class="modal-card" style="width: auto">
+            <header class="modal-card-head">
+                <p class="modal-card-title">ログイン</p>
+            </header>
+            <section class="modal-card-body">
+                <b-field label="Email">
+                    <b-input
+                            type="email"
+                            :value="email"
+                            placeholder="Your email"
+                            required>
+                    </b-input>
+                </b-field>
+
+                <b-field label="Password">
+                    <b-input
+                            type="password"
+                            :value="password"
+                            password-reveal
+                            placeholder="Your password"
+                            required>
+                    </b-input>
+                </b-field>
+
+                <b-checkbox>Remember me</b-checkbox>
+            </section>
+            <footer class="modal-card-foot">
+                <button class="button" type="button">新規登録</button>
+                <button class="button is-primary">ログイン</button>
+            </footer>
+        </div>
+    </form>
+</template>
+
+<script>
+    export default {
+    }
+</script>
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Index from './components/pages/index.vue'
+import Login from './components/pages/login.vue'
 import NotFound from './components/pages/notFound.vue'
+import Buefy from 'buefy'
+import 'buefy/lib/buefy.css'
 
 Vue.component('index', Index)
 
+
+Vue.use(Buefy)
 Vue.use(VueRouter)
 
 const routes = [
     { path: "/", component: Index },
+    { path: "/login" , component: Login },
     { path: "*", component: NotFound },
 ]
 const router = new VueRouter({  mode: 'history', routes })
